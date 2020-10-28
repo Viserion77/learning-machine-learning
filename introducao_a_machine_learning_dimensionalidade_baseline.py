610d91af165d582f08b997a55d84f8e5d920301b
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-10-27 22:07:52 -0300

    Alura Aula 4: agora com cruvatura

--- introducao_a_machine_learning_dimensionalidade_baseline.py
+++ introducao_a_machine_learning_dimensionalidade_baseline.py
@@ -109,3 +109,110 @@ plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y, s=1)
 plt.contourf(xx, yy, Z, alpha=0.3)
 plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y, s=1)
 
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = LinearSVC(random_state=SEED)
+modelo.fit(treino_x, treino_y)
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print("A acurácia foi %.2f%%" % acuracia)
+
+SEED = 5
+np.random.seed(SEED)
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,
+                                                         stratify = y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = LinearSVC()
+modelo.fit(treino_x, treino_y)
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print("A acurácia foi %.2f%%" % acuracia)
+
+from sklearn.svm import SVC
+
+SEED = 5
+np.random.seed(SEED)
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,
+                                                         stratify = y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = SVC()
+modelo.fit(treino_x, treino_y)
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print("A acurácia foi %.2f%%" % acuracia)
+
+x_min = teste_x.horas_esperadas.min()
+x_max = teste_x.horas_esperadas.max()
+y_min = teste_x.preco.min()
+y_max = teste_x.preco.max()
+
+pixels = 100
+eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixels)
+eixo_y = np.arange(y_min, y_max, (y_max - y_min) / pixels)
+
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+pontos = np.c_[xx.ravel(), yy.ravel()]
+
+Z = modelo.predict(pontos)
+Z = Z.reshape(xx.shape)
+
+import matplotlib.pyplot as plt
+
+plt.contourf(xx, yy, Z, alpha=0.3)
+plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y, s=1)
+
+# DECISION BOUNDARY
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+SEED = 5
+np.random.seed(SEED)
+raw_treino_x, raw_teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,
+                                                         stratify = y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+scaler = StandardScaler()
+scaler.fit(raw_treino_x)
+treino_x = scaler.transform(raw_treino_x)
+teste_x = scaler.transform(raw_teste_x)
+
+modelo = SVC()
+modelo.fit(treino_x, treino_y)
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print("A acurácia foi %.2f%%" % acuracia)
+
+data_x = teste_x[:,0]
+data_y = teste_x[:,1]
+
+x_min = data_x.min()
+x_max = data_x.max()
+y_min = data_y.min()
+y_max = data_y.max()
+
+pixels = 100
+eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixels)
+eixo_y = np.arange(y_min, y_max, (y_max - y_min) / pixels)
+
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+pontos = np.c_[xx.ravel(), yy.ravel()]
+
+Z = modelo.predict(pontos)
+Z = Z.reshape(xx.shape)
+
+import matplotlib.pyplot as plt
+
+plt.contourf(xx, yy, Z, alpha=0.3)
+plt.scatter(data_x, data_y, c=teste_y, s=1)
+
+# DECISION BOUNDARY
+
